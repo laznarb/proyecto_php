37c3ad0912896cec8f2bf6bcd4ee16aff20d01a8
laznarb <aa22.laznar44@iespabloserrano.com>
2023-11-09 12:51:05 +0100

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tienda de arte</h1>
+    <?php
+        //Realizo la conexión con la base de datos
+        include('conexion.php');
+        
+    
+    ?>
+</body>
+</html>
